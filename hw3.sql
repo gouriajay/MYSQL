5086ef124707616f098b56aa2cd63cd2e880b853
gouri <gouriajaygmail.com>
2025-12-09 20:26:07 +0530

    sent class work day3

--- hw3.sql
+++ hw3.sql
@@ -9,4 +9,10 @@ SELECT * FROM book WHERE Stock_status='In Stock' AND Price<400;
 SELECT * FROM book WHERE Stock_status='out of stock'OR price>700;
 SELECT Tittle,Price, Price*1.10 AS Price_with_gst FROM book;
 SELECT Tittle,Price,Stock_status FROM book ORDER BY Price DESC;
-​
+​SELECT * FROM `online book`
+CREATE TABLE `online`.`bookstore` (`ID` INT NOT NULL , `tittle` VARCHAR(20) NOT NULL , `author` VARCHAR(20) NOT NULL , `price` INT NOT NULL , `stock` INT NOT NULL ) ENGINE = InnoDB;
+INSERT INTO bookstore VALUES(1,'Learn SQL','John Smith',400,10), (2,'Mastering Python','Jane Doe',600,5), (3,'HTML&CSS Basics','Alan Webb',300,8);
+UPDATE bookstore SET price=price+50,stock=12 WHERE tittle='Learn SQL';
+UPDATE bookstore SET stock=stock-2 WHERE price>500;
+DELETE FROM bookstore WHERE id=3;
+
